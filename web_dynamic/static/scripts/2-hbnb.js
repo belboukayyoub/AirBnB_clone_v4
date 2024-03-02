d21784043d0050ca824f25589749d4628c398714
Belbouk Ayyoub <belboukayyoub@gmail.com>
2024-03-02 04:31:11 +0100

    0x06. Task 3. API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  const selectedAmenities = {};
+  $(".amenities .popover input").change(function () {
+    if ($(this).is(":checked")) {
+      selectedAmenities[$(this).data("id")] = $(this).data("name");
+    } else if ($(this).is(":not(:checked)")) {
+      delete selectedAmenities[$(this).data("id")];
+    }
+    const names = Object.values(selectedAmenities);
+    $(".amenities h4").text(names.sort().join(", "));
+  });
+
+  $.get(`http://127.0.0.1:5001/api/v1/status/`, (data) => {
+    if (data.status === "OK") {
+      $("#api_status").addClass("available");
+    } else {
+      $("#api_status").removeClass("available");
+    }
+  });
+});
